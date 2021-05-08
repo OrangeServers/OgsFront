87ee91583541200f79af826a859880e5455ed4b6
xuzhiwei <xuzhiwei@jinlinbao.com>
2021-05-08 17:07:16 +0800

    登录计时

--- js/login.js
+++ js/login.js
@@ -1,127 +1,127 @@
 function draw(show_num) {
-            var canvas_width = $('#canvas').width();
-            var canvas_height = $('#canvas').height();
-            var canvas = document.getElementById("canvas");//获取到canvas的对象，演员
-            var context = canvas.getContext("2d");//获取到canvas画图的环境，演员表演的舞台
-            canvas.width = canvas_width;
-            canvas.height = canvas_height;
-            var sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
-            var aCode = sCode.split(",");
-            var aLength = aCode.length;//获取到数组的长度
+    var canvas_width = $('#canvas').width();
+    var canvas_height = $('#canvas').height();
+    var canvas = document.getElementById("canvas");//获取到canvas的对象，演员
+    var context = canvas.getContext("2d");//获取到canvas画图的环境，演员表演的舞台
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+    var sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
+    var aCode = sCode.split(",");
+    var aLength = aCode.length;//获取到数组的长度
 
-            for (var i = 0; i <= 3; i++) {
-                var j = Math.floor(Math.random() * aLength);//获取到随机的索引值
-                var deg = Math.random() * 30 * Math.PI / 180;//产生0~30之间的随机弧度
-                var txt = aCode[j];//得到随机的一个内容
-                show_num[i] = txt.toLowerCase();
-                var x = 10 + i * 20;//文字在canvas上的x坐标
-                var y = 20 + Math.random() * 8;//文字在canvas上的y坐标
-                context.font = "bold 23px 微软雅黑";
+    for (var i = 0; i <= 3; i++) {
+        var j = Math.floor(Math.random() * aLength);//获取到随机的索引值
+        var deg = Math.random() * 30 * Math.PI / 180;//产生0~30之间的随机弧度
+        var txt = aCode[j];//得到随机的一个内容
+        show_num[i] = txt.toLowerCase();
+        var x = 10 + i * 20;//文字在canvas上的x坐标
+        var y = 20 + Math.random() * 8;//文字在canvas上的y坐标
+        context.font = "bold 23px 微软雅黑";
 
-                context.translate(x, y);
-                context.rotate(deg);
+        context.translate(x, y);
+        context.rotate(deg);
 
-                context.fillStyle = randomColor();
-                context.fillText(txt, 0, 0);
+        context.fillStyle = randomColor();
+        context.fillText(txt, 0, 0);
 
-                context.rotate(-deg);
-                context.translate(-x, -y);
-            }
-            for (var i = 0; i <= 5; i++) { //验证码上显示线条
-                context.strokeStyle = randomColor();
-                context.beginPath();
-                context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
-                context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
-                context.stroke();
-            }
-            for (var i = 0; i <= 30; i++) { //验证码上显示小点
-                context.strokeStyle = randomColor();
-                context.beginPath();
-                var x = Math.random() * canvas_width;
-                var y = Math.random() * canvas_height;
-                context.moveTo(x, y);
-                context.lineTo(x + 1, y + 1);
-                context.stroke();
-            }
-        }
+        context.rotate(-deg);
+        context.translate(-x, -y);
+    }
+    for (var i = 0; i <= 5; i++) { //验证码上显示线条
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.stroke();
+    }
+    for (var i = 0; i <= 30; i++) { //验证码上显示小点
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        var x = Math.random() * canvas_width;
+        var y = Math.random() * canvas_height;
+        context.moveTo(x, y);
+        context.lineTo(x + 1, y + 1);
+        context.stroke();
+    }
+}
 
-        function randomColor() {//得到随机的颜色值
-            var r = Math.floor(Math.random() * 256);
-            var g = Math.floor(Math.random() * 256);
-            var b = Math.floor(Math.random() * 256);
-            return "rgb(" + r + "," + g + "," + b + ")";
-        }
+function randomColor() {//得到随机的颜色值
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
 
 function user_login() {
-  // layer.alert($('.layui-form').serialize())
-       var logif = layer.load(1, {
-              shade: [0.1,'#fff'] //0.1透明度的白色背景
-          });
+    // layer.alert($('.layui-form').serialize())
+    var logif = layer.load(1, {
+        shade: [0.1, '#fff'] //0.1透明度的白色背景
+    });
     $.ajax({
-    type: "POST",
-    url: "http://10.0.1.198:18000/account/login_dl",
-    data: $('.layui-form').serialize(),
-    dataType: "JSON",
-    success: function (res) {
-        if (res['chk_status'] === 'true') {
-            window.location.href = '../index.html'
-            let date = new Date();
-            date.setTime(date.getTime()+4*60*60*1000);//只能这么写，10表示10秒钟
-            $.cookie('username',$("#orange-username").val(),{expires: date});
-        } else if (res['password_status'] === 'fail') {
-          layer.close(logif)
-          layer.alert('登录失败，密码或其他错误')
-        } else if (res['user_status'] === 'fail') {
-          layer.close(logif)
-          layer.alert('登录失败，用户名不存在')
+        type: "POST",
+        url: "http://10.0.1.198:18000/account/login_dl",
+        data: $('.layui-form').serialize(),
+        dataType: "JSON",
+        success: function (res) {
+            if (res['chk_status'] === 'true') {
+                window.location.href = '../index.html'
+                let date = new Date();
+                date.setTime(date.getTime() + 4 * 60 * 60 * 1000);//只能这么写，10表示10秒钟
+                $.cookie('username', $("#orange-username").val(), {expires: date});
+            } else if (res['password_status'] === 'fail') {
+                layer.close(logif)
+                layer.alert('登录失败，密码或其他错误')
+            } else if (res['user_status'] === 'fail') {
+                layer.close(logif)
+                layer.alert('登录失败，用户名不存在')
+            }
         }
-    }
     })
 }
 
 let show_num = [];
-$(function (){
+$(function () {
     draw(show_num);
     $("#canvas").on('click', function () {
         draw(show_num)
     })
 })
 
-layui.use(['form', 'layedit', 'laydate'], function(){
-  var form = layui.form
-  ,layer = layui.layer
-  ,layedit = layui.layedit
-  ,laydate = layui.laydate;
+layui.use(['form', 'layedit', 'laydate'], function () {
+    var form = layui.form
+        , layer = layui.layer
+        , layedit = layui.layedit
+        , laydate = layui.laydate;
 
-  //日期
+    //日期
     console.log($().cookie)
     laydate.render({
-      elem: '#date'
+        elem: '#date'
     });
     laydate.render({
-      elem: '#date1'
+        elem: '#date1'
     });
-    form.on('submit(demo1)', function(){
-    let val = $('#orange-yzm').val();
-    let num = show_num.join("");
-    console.log(val, num)
-      if (val !== num){
-          layer.msg('验证码错误', {icon: 5, time: 2000})
-          $('#orange-yzm').val('')
-          draw(show_num)
-      } else if (val === num){
-        user_login()
-      }
+    form.on('submit(demo1)', function () {
+        let val = $('#orange-yzm').val();
+        let num = show_num.join("");
+        console.log(val, num)
+        if (val !== num) {
+            layer.msg('验证码错误', {icon: 5, time: 2000})
+            $('#orange-yzm').val('')
+            draw(show_num)
+        } else if (val === num) {
+            user_login()
+        }
     });
 });
 
 // 测试监听输入事件
-function test_on_input(){
+function test_on_input() {
     let val = $('#orange-username').val()
-    if (val !== 'xuzhiwei'){
+    if (val !== 'xuzhiwei') {
         // layer.msg('????', {icon: 5, time: 2000})
         $("#orange-username").css('color', '#4cb450')
-    } else if (val === 'xuzhiwei'){
+    } else if (val === 'xuzhiwei') {
         $("#orange-username").css('color', '')
     }
 }
--- js/register.js
+++ js/register.js
@@ -1,9 +1,9 @@
- function chk_username(){
+function chk_username() {
     let user_val = $("#orange-username").val()
     if (user_val === '') {
         layer.msg('请输入用户名', {icon: 3})
     } else {
-    $.ajax({
+        $.ajax({
             type: "POST",
             url: "http://10.0.1.198:18000/account/chk_username",
             data: {
@@ -11,114 +11,119 @@
             },
             dataType: "JSON",
             success: function (res) {
-                if (res['chk_user_status'] === 'true'){
+                if (res['chk_user_status'] === 'true') {
                     layer.msg('该用户名可用', {icon: 1})
-                }else if (res['chk_user_status'] === 'fail'){
+                } else if (res['chk_user_status'] === 'fail') {
                     layer.msg('该用户名已存在', {icon: 2})
                 }
             }
-    })
-  }
- }
+        })
+    }
+}
 
- function send_mail(thisBtn) {
-    // 重置获取验证码按钮
-     function out_verification_time() {
-         var clock = '';
-         var nums = 60;
-         var btn;
-         btn = thisBtn;
-         btn.disabled = true; //将按钮置为不可点击
-         btn.innerHTML = nums + '秒后可重新获取';
-         clock = setInterval(doLoop, 1000); //一秒执行一次
-         function doLoop() {
-             nums--;
-             if (nums > 0) {
-                 btn.innerHTML = nums + '秒后可重新获取';
-             } else {
-                 clearInterval(clock); //清除js定时器
-                 btn.disabled = false;
-                 btn.innerHTML = '获取验证码';
-                 nums = 10; //重置时间
-             }
-         }
-     }
-
-    $.ajax({
-        type: "POST",
-        url: "http://10.0.1.198:18000/mail/send_user_mail",
-        data: {
-            'email': $("#email").val(),
-        },
-        dataType: "JSON",
-        success: function (res) {
-            if (res['send_status'] === 'true'){
-                layer.msg('发送成功', {icon: 1})
-                out_verification_time()
-            }else {
-                layer.msg('该邮箱已被注册', {icon: 2})
+function send_mail(thisBtn) {
+    let user_val = $("#email").val()
+    if (user_val === '') {
+        layer.msg('未输入邮箱', {icon: 3})
+    } else {
+        // 重置获取验证码按钮
+        function out_verification_time() {
+            var clock = '';
+            var nums = 60;
+            var btn;
+            btn = thisBtn;
+            btn.disabled = true; //将按钮置为不可点击
+            btn.innerHTML = nums + '秒后可重新获取';
+            clock = setInterval(doLoop, 1000); //一秒执行一次
+            function doLoop() {
+                nums--;
+                if (nums > 0) {
+                    btn.innerHTML = nums + '秒后可重新获取';
+                } else {
+                    clearInterval(clock); //清除js定时器
+                    btn.disabled = false;
+                    btn.innerHTML = '获取验证码';
+                    nums = 10; //重置时间
+                }
             }
         }
 
-    })
- }
+        $.ajax({
+            type: "POST",
+            url: "http://10.0.1.198:18000/mail/send_user_mail",
+            data: {
+                'email': user_val,
+            },
+            dataType: "JSON",
+            success: function (res) {
+                if (res['send_status'] === 'true') {
+                    layer.msg('发送成功', {icon: 1})
+                    out_verification_time()
+                } else {
+                    layer.msg('该邮箱已被注册', {icon: 2})
+                }
+            }
 
- function com_register(){
+        })
+    }
+}
+
+function com_register() {
     $.ajax({
-                type: "POST",
+        type: "POST",
         url: "http://10.0.1.198:18000/account/com_register",
         data: $(".layui-form").serialize(),
         dataType: "JSON",
         success: function (res) {
             console.log(res);
-            if (res['chk_user_status'] === 'fail'){
+            if (res['chk_user_status'] === 'fail') {
                 layer.msg('该用户名已存在,请检查后再试', {icon: 2})
-            } else if (res['verification'] === 'fail'){
+            } else if (res['verification'] === 'fail') {
                 layer.msg('验证码错误，请检查后再试', {icon: 2})
-            } else if (res['chk_mail_status'] === 'fail'){
+            } else if (res['chk_mail_status'] === 'fail') {
                 layer.msg('邮箱已被注册，请检查后再试', {icon: 2})
-            } else if (res['chk_verification'] === 'fail'){
+            } else if (res['chk_verification'] === 'fail') {
                 layer.msg('验证码已过期，请重新获取', {icon: 2})
-            } else if (res['chk_user_status'] === 'true' && res['chk_mail_status'] === 'true' && res['verification'] === 'true'){
+            } else if (res['chk_user_status'] === 'true' && res['chk_mail_status'] === 'true' && res['verification'] === 'true') {
                 layer.msg('注册成功', {icon: 1})
                 window.location.href = "../login.html"
             }
         }
     })
- }
+}
 
- var foo = function(element) {
-  var intent = document.querySelector(element)
-  intent.disabled = 'disabled'
-  setTimeout(function() {
-    intent.disabled = ''
-  }, 30000) /* 可以把 3000 改成自定义的时间*/
-  console.log('30 秒内不可以重复点击')
+var foo = function (element) {
+    var intent = document.querySelector(element)
+    intent.disabled = 'disabled'
+    setTimeout(function () {
+        intent.disabled = ''
+    }, 30000) /* 可以把 3000 改成自定义的时间*/
+    console.log('30 秒内不可以重复点击')
 }
 
 
-var bar = function(fun, select) {
-  var intent = document.querySelector('#check_name')
-  intent.addEventListener('click', function() {
-    fun(select)
-  })
+var bar = function (fun, select) {
+    var intent = document.querySelector('#check_name')
+    intent.addEventListener('click', function () {
+        fun(select)
+    })
 }
 
-layui.use(['form', 'layedit', 'laydate'], function(){
-  var form = layui.form
-  ,layer = layui.layer
-  ,layedit = layui.layedit
-  ,laydate = layui.laydate;
+layui.use(['form', 'layedit', 'laydate'], function () {
+    var form = layui.form
+        , layer = layui.layer
+        , layedit = layui.layedit
+        , laydate = layui.laydate;
 
-  //日期
+    //日期
     console.log($().cookie)
     laydate.render({
-      elem: '#date'
+        elem: '#date'
     });
     laydate.render({
-      elem: '#date1'
+        elem: '#date1'
     });
-    form.on('submit(demo1)', function(){
+    form.on('submit(demo1)', function () {
         com_register()
     });
 });
