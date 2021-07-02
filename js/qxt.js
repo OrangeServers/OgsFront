83a9363807b080ea41f37de0d3b6a97965472fc5
xuzhiwei <xuzhiwei@jinlinbao.com>
2021-07-02 10:37:59 +0800

    更新折线图展示

--- js/qxt.js
+++ js/qxt.js
@@ -32,12 +32,8 @@ function get_count_list() {
         }
     })
 }
-
-
 //折线图js文件
-am4core.ready(function () {
-
-// Themes begin
+am4core.ready(function() {
 
     get_user_auth_list('index')
 
@@ -54,122 +50,94 @@ am4core.ready(function () {
             console.log(res)
         }
     })
+
+// Themes begin
+// Themes end
+
+    $.ajax({
+        type: "POST",
+        url: ogs_backend_url + "/local/chart/count",
+        dataType: "JSON",
+        async: false,
+        success: function (res) {
+            window.login_data = res['login_msg']
+            window.user_data = res['user_msg']
+        }
+    })
+
+
 // Create chart instance
-    var chart = am4core.create("zhexiandiv", am4charts.XYChart);
+var chart = am4core.create("zhexiandiv", am4charts.XYChart);
 
 // Enable chart cursor
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.lineX.disabled = true;
-    chart.cursor.lineY.disabled = true;
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.lineX.disabled = true;
+chart.cursor.lineY.disabled = true;
 
 // Enable scrollbar
-    chart.scrollbarX = new am4core.Scrollbar();
+chart.scrollbarX = new am4core.Scrollbar();
 
 // Add data
-
-    $.ajax({
-        type: "POST",
-        url: ogs_backend_url + "/local/chart/count",
-        dataType: "JSON",
-        async: false,
-        success: function (res) {
-            chart.data = res['login_msg']
-        }
-    })
-
-    // chart.data = [{
-    //     "date": "2012-01-01",
-    //     "value": 8
-    // }, {
-    //     "date": "2012-01-02",
-    //     "value": 10
-    // }];
+chart.data = window.login_data
 
 // Create axes
-    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0.5;
-    dateAxis.dateFormatter.inputDateFormat = "yyyy-MM-dd";
-    dateAxis.renderer.minGridDistance = 40;
-    dateAxis.tooltipDateFormat = "MMM dd, yyyy";
-    dateAxis.dateFormats.setKey("day", "dd");
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.grid.template.location = 0.5;
+dateAxis.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+dateAxis.renderer.minGridDistance = 40;
+dateAxis.tooltipDateFormat = "MMM dd, yyyy";
+dateAxis.dateFormats.setKey("day", "dd");
 
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
 // Create series
-    var series = chart.series.push(new am4charts.LineSeries());
-    series.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
-    series.dataFields.valueY = "value";
-    series.dataFields.dateX = "date";
-    series.strokeDasharray = 3;
-    series.strokeWidth = 2
-    series.strokeOpacity = 0.3;
-    series.strokeDasharray = "3,3"
+var series = chart.series.push(new am4charts.LineSeries());
+series.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
+series.dataFields.valueY = "value";
+series.dataFields.dateX = "date";
+series.strokeDasharray = 3;
+series.strokeWidth = 2
+series.strokeOpacity = 0.3;
+series.strokeDasharray = "3,3"
 
-    var bullet = series.bullets.push(new am4charts.CircleBullet());
-    bullet.strokeWidth = 2;
-    bullet.stroke = am4core.color("#fff");
-    bullet.setStateOnChildren = true;
-    bullet.propertyFields.fillOpacity = "opacity";
-    bullet.propertyFields.strokeOpacity = "opacity";
+var bullet = series.bullets.push(new am4charts.CircleBullet());
+bullet.strokeWidth = 2;
+bullet.stroke = am4core.color("#fff");
+bullet.setStateOnChildren = true;
+bullet.propertyFields.fillOpacity = "opacity";
+bullet.propertyFields.strokeOpacity = "opacity";
 
-    var hoverState = bullet.states.create("hover");
-    hoverState.properties.scale = 1.7;
+var hoverState = bullet.states.create("hover");
+hoverState.properties.scale = 1.7;
 
-    function createTrendLine(data) {
-        var trend = chart.series.push(new am4charts.LineSeries());
-        trend.dataFields.valueY = "value";
-        trend.dataFields.dateX = "date";
-        trend.strokeWidth = 2
-        trend.stroke = trend.fill = am4core.color("#4cb450");
-        trend.data = data;
+function createTrendLine(data) {
+  var trend = chart.series.push(new am4charts.LineSeries());
+  trend.dataFields.valueY = "value";
+  trend.dataFields.dateX = "date";
+  trend.strokeWidth = 2
+  trend.stroke = trend.fill = am4core.color("#4cb450");
+  trend.data = data;
 
-        var bullet = trend.bullets.push(new am4charts.CircleBullet());
-        bullet.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
-        bullet.strokeWidth = 2;
-        bullet.stroke = am4core.color("#fff")
-        bullet.circle.fill = trend.stroke;
+  var bullet = trend.bullets.push(new am4charts.CircleBullet());
+  bullet.tooltipText = "{date}\n[bold font-size: 17px]value: {valueY}[/]";
+  bullet.strokeWidth = 2;
+  bullet.stroke = am4core.color("#fff")
+  bullet.circle.fill = trend.stroke;
 
-        var hoverState = bullet.states.create("hover");
-        hoverState.properties.scale = 1.7;
+  var hoverState = bullet.states.create("hover");
+  hoverState.properties.scale = 1.7;
 
-        return trend;
-    };
+  return trend;
+};
 
-    $.ajax({
-        type: "POST",
-        url: ogs_backend_url + "/local/chart/count",
-        dataType: "JSON",
-        async: false,
-        success: function (res) {
-            createTrendLine(res['user_msg'])
-            window.lastTrend = createTrendLine
-        }
-    })
-
-    // createTrendLine([
-    //     {"date": "2012-01-02", "value": 1},
-    //     {"date": "2012-01-03", "value": 2},
-    //     {"date": "2012-01-04", "value": 3},
-    //     {"date": "2012-01-05", "value": 4},
-    //     {"date": "2012-01-06", "value": 3},
-    //     {"date": "2012-01-11", "value": 6}
-    // ]);
-    //
-    // var lastTrend = createTrendLine([
-    //     {"date": "2012-01-17", "value": 16},
-    //     {"date": "2012-01-22", "value": 10}
-    // ]);
-
-    let mydata = new Date()
-    let day = mydata.getDate();
-    let month = mydata.getMonth() + 1
-    let year = mydata.getFullYear()
-    console.log(day,month,year)
-
-// Initial zoom once chart is ready
-        window.lastTrend.events.once("datavalidated", function(){
-          series.xAxis.zoomToDates(new Date(year, month, day), new Date(year, month, day));
-        });
+createTrendLine(window.user_data);
+// let lastTrend = createTrendLine()
+// // var lastTrend = createTrendLine(window.user_data);
+//
+// // Initial zoom once chart is ready
+// lastTrend.events.once("datavalidated", function(){
+//   series.xAxis.zoomToDates(new Date(2021, 7, 2), new Date(2021, 7, 13));
+// });
 
 }); // end am4core.ready()
 
