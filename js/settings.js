232b0734d88281b09b043f0dae50b9bd4d6010f7
xuwei777 <954157605@qq.com>
2021-07-19 16:22:03 +0800

    新增每个用户有独立配置文件,但是登录失效时间和开启用户注册暂时只admin生效

--- js/settings.js
+++ js/settings.js
@@ -10,7 +10,7 @@ layui.use(['form', 'jquery', 'layedit', 'laydate', 'element'], function () {
     $.ajax({
         type: "POST",
         url: ogs_backend_url + "/local/settings/get",
-        data: {'name': 'admin'},
+        data: {'name': $.cookie('username')},
         dataType: "JSON",
         success: function (res) {
             $(".orange-sel" + res['login_time']).attr('selected', '')
@@ -21,7 +21,7 @@ layui.use(['form', 'jquery', 'layedit', 'laydate', 'element'], function () {
     })
 
     function settings_update() {
-        let data = $('.layui-form').serialize() + "&name=admin"
+        let data = $('.layui-form').serialize() + "&name=" + $.cookie('username')
         $.ajax({
             type: "POST",
             url: ogs_backend_url + "/local/settings/update",
