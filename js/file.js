84b55c28d2eeb457486614db1ed19f681078bfe8
xuwei777 <954157605@qq.com>
2021-07-27 16:05:23 +0800

    删除旧文件，将js内容整合到js文件里

--- js/file.js
+++ js/file.js
@@ -1,232 +1,201 @@
-layui.use(['upload', 'element', 'layer'], function () {
-    var $ = layui.jquery
-        , upload = layui.upload
-        , element = layui.element
-        , layer = layui.layer;
+layui.use(['upload', 'dropdown', 'util', 'layer', 'table', 'element'], function () {
+    let element = layui.element,
+        upload = layui.upload,
+        dropdown = layui.dropdown,
+        util = layui.util,
+        layer = layui.layer,
+        table = layui.table,
+        $ = layui.jquery;
 
     get_user_auth_list('filetransfer')
 
-    //常规使用 - 普通图片上传
-    var uploadInst = upload.render({
-        elem: '#test1'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , before: function (obj) {
-            //预读本地文件示例，不支持ie8
-            obj.preview(function (index, file, result) {
-                $('#demo1').attr('src', result); //图片链接（base64）
-            });
+    // $("#demo6").dblclick(function (){
+    //     layer.msg($(this).val())
+    // })
 
-            element.progress('demo', '0%'); //进度条复位
-            layer.msg('上传中', {icon: 16, time: 0});
-        }
-        , done: function (res) {
-            //如果上传失败
-            if (res.code > 0) {
-                return layer.msg('上传失败');
-            }
-            //上传成功的一些操作
-            //……
-            $('#demoText').html(''); //置空上传失败的状态
-        }
-        , error: function () {
-            //演示失败状态，并实现重传
-            var demoText = $('#demoText');
-            demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
-            demoText.find('.demo-reload').on('click', function () {
-                uploadInst.upload();
-            });
-        }
-        //进度条
-        , progress: function (n, index, e) {
-            element.progress('demo', n + '%'); //可配合 layui 进度条元素使用
-            if (n == 100) {
-                layer.msg('上传完毕', {icon: 1});
-            }
-        }
-    });
-
-    //多图片上传
-    upload.render({
-        elem: '#test2'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , multiple: true
-        , before: function (obj) {
-            //预读本地文件示例，不支持ie8
-            obj.preview(function (index, file, result) {
-                $('#demo2').append('<img src="' + result + '" alt="' + file.name + '" class="layui-upload-img">')
-            });
-        }
-        , done: function (res) {
-            //上传完毕
-        }
-    });
-
-    //指定允许上传的文件类型
-    upload.render({
-        elem: '#test3'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'file' //普通文件
-        , before: function (obj) {
-            //预读本地文件示例，不支持ie8
-            // obj.preview(function(index, file, result){
-            //   $('#demo1').attr('src', result); //图片链接（base64）
-            // });
-
-            element.progress('demo', '0%'); //进度条复位
-            layer.msg('上传中', {icon: 16, time: 0});
-        }
-        , done: function (res) {
-            // layer.msg('上传成功');
-            console.log(res);
-        }
-        , progress: function (n, index, e) {
-            element.progress('demo', n + '%'); //可配合 layui 进度条元素使用
-            if (n == 100) {
-                layer.msg('上传完毕', {icon: 1});
-            }
-        }
-    });
-    upload.render({ //允许上传的文件后缀
-        elem: '#test4'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'file' //普通文件
-        , exts: 'zip|rar|7z' //只允许上传压缩文件
-        , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
-        }
-    });
-    upload.render({
-        elem: '#test5'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'video' //视频
-        , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
-        }
-    });
-    upload.render({
-        elem: '#test6'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'audio' //音频
-        , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
-        }
-    });
-
-    //设定文件大小限制
-    upload.render({
-        elem: '#test7'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , size: 60 //限制文件大小，单位 KB
-        , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
-        }
-    });
-
-    //同时绑定多个元素，并将属性设定在元素上
-    upload.render({
-        elem: '.demoMore'
-        , before: function () {
-            layer.tips('接口地址：' + this.url, this.item, {tips: 1});
-        }
-        , done: function (res, index, upload) {
-            var item = this.item;
-            console.log(item); //获取当前触发上传的元素，layui 2.1.0 新增
+    $(".orange-btn-checkout").on('click', function () {
+        if (window.file_ispath === '/') {
+            file_render('/')
+            layer.msg('已经到根目录了')
+        } else {
+            file_render(window.file_ispath, 'checkout')
         }
     })
 
-    //选完文件后不自动上传
-    upload.render({
-        elem: '#test8'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , auto: false
-        //,multiple: true
-        , bindAction: '#test9'
-        , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
+    $(".orange-btn-root").on('click', function () {
+        file_render('/')
+    })
+
+    // $(".orange-btn-file").on('click', function () {
+    //     put_file_render($.cookie('file_ispath'))
+    // })
+
+    let put_file = upload.render({
+        elem: '#test3'
+        , url: ogs_backend_url + "/local/file/put" //改成您自己的上传接口
+        , accept: 'file' //普通文件
+        , auto: true
+        , before: function (obj) {
+            element.progress('demo-jdt', '0%'); //进度条复位
         }
-    });
-
-    //拖拽上传
-    upload.render({
-        elem: '#test10'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'file'
-        , done: function (res) {
-            layer.msg('上传成功');
-            layui.$('#uploadDemoView').removeClass('layui-hide').find('img').attr('src', res.files.file);
-            console.log(res)
-        }
-    });
-
-    //多文件列表示例
-    var demoListView = $('#demoList')
-        , uploadListIns = upload.render({
-        elem: '#testList'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
-        , accept: 'file'
-        , multiple: true
-        , auto: false
-        , bindAction: '#testListAction'
-        , choose: function (obj) {
-            var files = this.files = obj.pushFile(); //将每次选择的文件追加到文件队列
-            //读取本地文件
-            obj.preview(function (index, file, result) {
-                var tr = $(['<tr id="upload-' + index + '">'
-                    , '<td>' + file.name + '</td>'
-                    , '<td>' + (file.size / 1024).toFixed(1) + 'kb</td>'
-                    , '<td>等待上传</td>'
-                    , '<td>'
-                    , '<button class="layui-btn layui-btn-xs demo-reload layui-hide">重传</button>'
-                    , '<button class="layui-btn layui-btn-xs layui-btn-danger demo-delete">删除</button>'
-                    , '</td>'
-                    , '</tr>'].join(''));
-
-                //单个重传
-                tr.find('.demo-reload').on('click', function () {
-                    obj.upload(index, file);
-                });
-
-                //删除
-                tr.find('.demo-delete').on('click', function () {
-                    delete files[index]; //删除对应的文件
-                    tr.remove();
-                    uploadListIns.config.elem.next()[0].value = ''; //清空 input file 值，以免删除后出现同名文件不可选
-                });
-
-                demoListView.append(tr);
-            });
-        }
-        , done: function (res, index, upload) {
-            if (res.files.file) { //上传成功
-                var tr = demoListView.find('tr#upload-' + index)
-                    , tds = tr.children();
-                tds.eq(2).html('<span style="color: #5FB878;">上传成功</span>');
-                tds.eq(3).html(''); //清空操作
-                return delete this.files[index]; //删除文件队列已经上传成功的文件
+        , progress: function (n, elem, res, index) {
+            element.progress('demo-jdt', n + '%'); //可配合 layui 进度条元素使用
+            if (n === 100) {
+                layer.msg('上传完毕', {icon: 1});
             }
-            this.error(index, upload);
         }
-        , error: function (index, upload) {
-            var tr = demoListView.find('tr#upload-' + index)
-                , tds = tr.children();
-            tds.eq(2).html('<span style="color: #FF5722;">上传失败</span>');
-            tds.eq(3).find('.demo-reload').removeClass('layui-hide'); //显示重传
-        }
-    });
-
-    //绑定原始文件域
-    upload.render({
-        elem: '#test20'
-        , url: ogs_backend_url + '/local/file' //改成您自己的上传接口
+        , data: {'req_dir': $.cookie('file_ispath')}
         , done: function (res) {
-            layer.msg('上传成功');
-            console.log(res)
+            if (res['status'] === 'true') {
+                file_render($.cookie('file_ispath'))
+            }
         }
     });
 
-});
+    $(".orange-btn-createdir").on('click', function () {
+        layer.prompt({
+            formType: 0,
+            value: '新建文件夹',
+            title: '请输入文件夹名称',
+            btn: ['确定', '取消'], //按钮，
+            btnAlign: 'c'
+        }, function (value, index) {
+            $.ajax({
+                type: "POST",
+                url: ogs_backend_url + "/local/file/add",
+                dataType: "JSON",
+                data: {'req_dir': window.file_ispath, 'mk_filename': value},
+                success: function (res) {
+                    if (res['status'] === 'true') {
+                        layer.close(index)
+                        file_render(window.file_ispath)
+                    } else if (res['status'] === 'fail') {
+                        layer.msg(res['msg'])
+                    }
+                }
+            })
+        });
+    })
+
+    $(".orange-btn-reload").on('click', function () {
+        file_render(window.file_ispath)
+    })
+
+    function file_render(file_path, get_type = 'get') {
+        $(".file-div").html('')
+        $.ajax({
+            type: "POST",
+            url: ogs_backend_url + "/local/file/def_get",
+            dataType: "JSON",
+            data: {'req_dir': file_path, 'get_file_type': get_type},
+            success: function (res) {
+                //console.log(res)
+                window.file_ispath = res['ispath']
+                $.cookie('file_ispath', file_ispath)
+                $('.orange-btn-path').html('当前目录路径: ' + window.file_ispath)
+                // 重载上传文件渲染
+                put_file.reload({
+                    data: {'req_dir': file_ispath}
+                })
+                //指定允许上传的文件类型
+                let res_dir = res['dir']
+                let res_file = res['file']
+                for (let i = 0; i < res_dir.length; i++) {
+                    let html = '                <button class="layui-btn layui-btn-primary orange-dir" value="' + res_dir[i] + '" id="orange-dir-' + i + '">\n' +
+                        '                    <img src="image/文件夹2.png">\n' +
+                        '                    <p>' + res_dir[i] + '</p>\n' +
+                        '                </button>'
+                    $(".file-div").append(html)
+                    dropdown.render({
+                        elem: "#orange-dir-" + i //也可绑定到 document，从而重置整个右键
+                        , trigger: 'contextmenu' //contextmenu
+                        , isAllowSpread: false //禁止菜单组展开收缩
+                        , style: 'width: 200px' //定义宽度，默认自适应
+                        , id: 'test788' //定义唯一索引
+                        , data: [{
+                            title: '进入'
+                            , id: 'load'
+                        }, {
+                            title: '重命名'
+                            , id: 'rename'
+                        }, {
+                            title: '删除'
+                            , id: 'del'
+                        }]
+                        , click: function (obj, othis) {
+                            if (obj.id === 'load') {
+                                file_render(res['ispath'] + res_dir[i])
+                            } else if (obj.id === 'print') {
+                                window.print();
+                            } else if (obj.id === 'del') {
+                                $.ajax({
+                                    type: "POST",
+                                    url: ogs_backend_url + "/local/file/del",
+                                    dataType: "JSON",
+                                    data: {'req_dir': window.file_ispath, 'rm_filename': res_dir[i]},
+                                    success: function (res) {
+                                        if (res['status'] === 'true') {
+                                            file_render(window.file_ispath)
+                                        }
+                                    }
+                                })
+                            }
+                        }
+                    });
+                    $("#" + "orange-dir-" + i).dblclick(function () {
+                        //console.log($(this).val())
+                        file_render(res['ispath'] + res_dir[i])
+                    })
+                }
+
+                for (let y = 0; y < res_file.length; y++) {
+                    let html = '                <button class="layui-btn layui-btn-primary orange-dir" id="orange-file-' + y + '">\n' +
+                        '                    <img src="image/文件2.png">\n' +
+                        '                    <p>' + res_file[y] + '</p>\n' +
+                        '                </button>'
+                    $(".file-div").append(html)
+                    dropdown.render({
+                        elem: "#orange-file-" + y //也可绑定到 document，从而重置整个右键
+                        , trigger: 'contextmenu' //contextmenu
+                        , isAllowSpread: false //禁止菜单组展开收缩
+                        , style: 'width: 200px' //定义宽度，默认自适应
+                        , id: 'test799' //定义唯一索引
+                        , data: [{
+                            title: '下载'
+                            , id: 'dow'
+                        }, {
+                            title: '重命名'
+                            , id: 'rename'
+                        }, {
+                            title: '删除'
+                            , id: 'del'
+                        }]
+                        , click: function (obj, othis) {
+                            if (obj.id === 'dow') {
+                                window.open('http://10.0.1.198:18000/local/file/download?filename=' + window.file_ispath + res_file[y])
+                            } else if (obj.id === 'rename') {
+                                window.print();
+                            } else if (obj.id === 'del') {
+                                $.ajax({
+                                    type: "POST",
+                                    url: ogs_backend_url + "/local/file/del",
+                                    dataType: "JSON",
+                                    data: {'req_dir': window.file_ispath, 'rm_filename': res_file[y]},
+                                    success: function (res) {
+                                        if (res['status'] === 'true') {
+                                            file_render(window.file_ispath)
+                                        }
+                                    }
+                                })
+                            }
+                        }
+                    });
+                }
+            }
+        })
+    }
+
+    file_render('/')
+
+
+})
