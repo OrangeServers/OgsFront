9dcefc410cf01d4dc941a5ec15ea5c5c8113ba2f
xuwei777 <954157605@qq.com>
2021-08-25 15:28:32 +0800

    新增资产提交不跳转并继续可提交功能，修改取消按钮样式

--- property/property-hostlist/add.js
+++ property/property-hostlist/add.js
@@ -1,31 +1,3 @@
-function host_add() {
-    // layer.alert($('.layui-form').serialize())
-    var logif = layer.load(1, {
-        shade: [0.1, '#fff'] //0.1透明度的白色背景
-    });
-    let data = $('.layui-form').serialize() + "&cz_name=" + $.cookie('username')
-    $.ajax({
-        type: "POST",
-        url: ogs_backend_url + "/server/host/add",
-        data: data,
-        dataType: "JSON",
-        success: function (res) {
-            if (res['server_add_status'] === 'true') {
-                window.location.href = '/property/property-hostlist.html'
-            } else if (res['server_add_status'] === 'fail') {
-                layer.close(logif)
-                layer.alert('添加失败，密码或其他错误')
-            } else if (res['server_add_status'] === 'con_fail') {
-                layer.close(logif)
-                layer.alert('添加失败，主机无法连接')
-            } else if (res['server_add_status'] === 'sel_fail') {
-                layer.close(logif)
-                layer.alert('添加失败，该主机已存在')
-            }
-        }
-    })
-}
-
 layui.use(['form', 'layedit', 'laydate', 'element'], function () {
     var form = layui.form
         , layer = layui.layer
@@ -38,6 +10,39 @@ layui.use(['form', 'layedit', 'laydate', 'element'], function () {
     // 获取组名列表
     get_group_name_list()
 
+    function host_add(add_type) {
+        // layer.alert($('.layui-form').serialize())
+        var logif = layer.load(1, {
+            shade: [0.1, '#fff'] //0.1透明度的白色背景
+        });
+        let data = $('.layui-form').serialize() + "&cz_name=" + $.cookie('username')
+        $.ajax({
+            type: "POST",
+            url: ogs_backend_url + "/server/host/add",
+            data: data,
+            dataType: "JSON",
+            success: function (res) {
+                if (res['server_add_status'] === 'true') {
+                    if (add_type === 'save') {
+                        window.location.href = '/property/property-hostlist.html'
+                    } else if (add_type === 'save_jx') {
+                        layer.close(logif)
+                        layer.msg('添加成功', {icon: 1, time: 2000})
+                    }
+                } else if (res['server_add_status'] === 'fail') {
+                    layer.close(logif)
+                    layer.alert('添加失败，密码或其他错误')
+                } else if (res['server_add_status'] === 'con_fail') {
+                    layer.close(logif)
+                    layer.alert('添加失败，主机无法连接')
+                } else if (res['server_add_status'] === 'sel_fail') {
+                    layer.close(logif)
+                    layer.alert('添加失败，该主机已存在')
+                }
+            }
+        })
+    }
+
     //日期
     laydate.render({
         elem: '#date'
@@ -46,6 +51,11 @@ layui.use(['form', 'layedit', 'laydate', 'element'], function () {
         elem: '#date1'
     });
     form.on('submit(demo1)', function () {
-        host_add()
+        host_add('save')
     });
+
+    form.on('submit(demo2)', function () {
+        host_add('save_jx')
+    });
+
 });
